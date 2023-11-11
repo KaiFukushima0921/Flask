ec2198953c8ab65b544cda5ae5d5731598b29de2
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-11 10:02:27 +0900

    flaskチュートリアルDay1-3再学習

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,21 +1,21 @@
 from flask import Flask,render_template,request
 
-# Flaskオブジェクトの作成
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    name = request.args.get("name")
+    show_name = request.args.get("a")
     okyo = ["色不異空","空不異色","色即是空","空即是色"]
-    return render_template("index.html",name=name,okyo=okyo)
+    return render_template("index.html",name=show_name,ok=okyo)
 
 
 @app.route("/index", methods=["post"])
 def post():
-    name = request.form["name"]
+    name = request.form["name2"]
     okyo = ["色不異空", "空不異色", "色即是空", "空即是色"]
     return render_template("index.html", name = name, okyo = okyo)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
