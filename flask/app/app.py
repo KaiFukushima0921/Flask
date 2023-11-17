aa7fd472fb0193394a83ad9fa56e40bba73d1eb0
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-17 21:56:49 +0900

    day5途中まで

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request
 from .models.models import OnegaiContent
+from .models.database import db_session
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,6 +19,15 @@ def post():
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html", name = name, all_onegai = all_onegai)
 
+
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
 
 if __name__ == "__main__":
     app.run(debug=True)
