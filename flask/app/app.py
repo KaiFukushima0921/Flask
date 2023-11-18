cb39d0f20c9137c9bfb36758303873d3df6029e0
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-18 14:31:24 +0900

    day5完了

--- flask/app/app.py
+++ flask/app/app.py
@@ -29,5 +29,24 @@ def add():
     db_session.commit()
     return index()
 
+
+@app.route("/update",methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return index()
+
+
+@app.route("/delete",methods=["post"])
+def delete():
+    id_list = request.form.getlist("delete")
+    for id in id_list:
+        content = OnegaiContent.query.filter_by(id=id).first()
+        db_session.delete(content)
+    db_session.commit()
+    return index()
+
 if __name__ == "__main__":
     app.run(debug=True)
